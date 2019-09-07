9f0b08be643e28ca80ac64c7a36b761925448d6e
Tee <teedia31@gmail.com>
2019-09-07 19:35:59 -0400

    Update Example Model - Using explicit type properties for curl script demo

--- app/models/example.js
+++ app/models/example.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose')
 // assign a new Mongoose Schema with
 // attributes in here.
 const exampleSchema = new mongoose.Schema({
-  name: {
-    firstName: String,
-    lastName: String
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
   }
 }, {
   timestamps: true
