087878a6bc6d5213541a44f5c190fdd128970137
Tee <teedia31@gmail.com>
2019-09-08 12:57:55 -0400

    Update removeBlankFields.js
    - Added middleware documentation

--- app/middleware/removeBlankFields.js
+++ app/middleware/removeBlankFields.js
@@ -1,18 +1,24 @@
-// this is a middleware for removing any key/value pairs from `req.body.foo`
-// that have an empty string as a value, e.g.
-// { example: { title: 'thing', text: '' } } -> { example: { title: 'thing' } }
-module.exports = function (req, res, next) {
+/**
+ * Middleware for removing any key/value pairs from `req.body.foo`
+ * that have an empty string as a value, e.g.
+ * { example: { first_name: 'Buttercup', last_name: '' } } -> { example: { first_name: 'Buttercup' } }
+ * @param {Object} req request object
+ * @param {Object} res response object
+ * @param {Function} next next function
+ */
+const removeBlanks = (req, res, next) => {
   // we don't know the name of the object in `req.body`, so we'll apply this to
   // ALL objects in `req.body`
   Object.values(req.body).forEach(obj => {
     for (const key in obj) {
       if (obj[key] === '') {
-        // removes both the key and the value, preventing it from being updated
+        // prevent the field from being updated
+        // by removing both the key and the value
         delete obj[key]
       }
     }
+    next()
   })
-
-  // pass `req` and `res` on to the route handler
-  next()
 }
+
+module.exports = removeBlanks
